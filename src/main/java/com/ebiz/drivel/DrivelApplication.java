4fd03a5981a9d8eaf47fef8d4a797ecca868535c
이승우 <swoolee97@ajou.ac.kr>
2024-05-28 15:47:41 +0900

    프로젝트 생성

---
+++ src/main/java/com/ebiz/drivel/DrivelApplication.java
@@ -0,0 +1,13 @@
+package com.ebiz.drivel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrivelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ebiz/drivel/DrivelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebiz.drivel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
