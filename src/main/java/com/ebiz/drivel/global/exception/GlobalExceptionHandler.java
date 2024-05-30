7e0c32cc931986a545400a394c565a90f0688c59
이승우 <swoolee97@ajou.ac.kr>
2024-05-30 17:50:07 +0900

    add: 404에러 핸들러 추가

--- src/main/java/com/ebiz/drivel/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ebiz/drivel/global/exception/GlobalExceptionHandler.java
@@ -5,15 +5,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     private static final String INCLUDED_NULL_DATA_EXCEPTION_MESSAGE = "빈 값이 있습니다";
     private static final String WRONG_USER_EXCEPTION_MESSAGE = "이메일 혹은 비밀번호가 일치하지 않습니다";
+    private static final String WRONG_REQUEST_EXCEPTION_MESSAGE = "잘못된 요청입니다";
 
     @ExceptionHandler({BadCredentialsException.class, UsernameNotFoundException.class})
     public ResponseEntity handleBadCredentialsException() {
@@ -28,5 +31,13 @@ public class GlobalExceptionHandler {
                 .message(INCLUDED_NULL_DATA_EXCEPTION_MESSAGE)
                 .build(), HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler({NoHandlerFoundException.class, HttpRequestMethodNotSupportedException.class})
+    public ResponseEntity handleNotFoundException() {
+        return new ResponseEntity(ErrorResponse.builder()
+                .message(WRONG_REQUEST_EXCEPTION_MESSAGE)
+                .build(), HttpStatus.NOT_FOUND);
+    }
+
 
 }
