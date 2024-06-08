c83c52bd3e086a86e96d6eb70ee58edc87bf249c
이승우 <swoolee97@ajou.ac.kr>
2024-06-09 04:11:28 +0900

    refactor: 카카오 redirect uri 배포용으로 수정

--- src/main/java/com/ebiz/drivel/domain/kakao/service/KakaoService.java
+++ src/main/java/com/ebiz/drivel/domain/kakao/service/KakaoService.java
@@ -6,6 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
@@ -14,21 +17,26 @@ import reactor.core.publisher.Flux;
 public class KakaoService {
     @Value("${KAKAO_RESTAPI_KEY}")
     private String CLIENT_ID;
+    @Value("${KAKAO_REDIRECT_URI}")
+    private String REDIRECT_URI;
     private final WebClient webClient;
 
     private static final String TOKEN_URI = "https://kauth.kakao.com/oauth/token";
-    private static final String REDIRECT_URI = "http://localhost:8080/kakao/callback";
+    //    private static final String REDIRECT_URI = "http://localhost:8080/kakao/callback";
     private static final String GRANT_TYPE = "authorization_code";
     private static final String USER_INFO_URI = "https://kapi.kakao.com/v2/user/me";
 
     public KakaoTokenResponse getToken(String code) {
-        String uri =
-                TOKEN_URI + "?grant_type=" + GRANT_TYPE + "&client_id=" + CLIENT_ID + "&redirect_uri=" + REDIRECT_URI
-                        + "&code=" + code;
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", GRANT_TYPE);
+        params.add("redirect_uri", REDIRECT_URI);
+        params.add("client_id", CLIENT_ID);
+        params.add("code", code);
 
         Flux<KakaoTokenResponse> response = webClient.post()
-                .uri(uri)
-                .contentType(MediaType.APPLICATION_JSON)
+                .uri(TOKEN_URI)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(BodyInserters.fromFormData(params))
                 .retrieve()
                 .bodyToFlux(KakaoTokenResponse.class);
 
