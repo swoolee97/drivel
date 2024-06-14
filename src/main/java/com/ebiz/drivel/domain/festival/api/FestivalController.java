c691c92f5a2fbf88a9e8eef1e0999ec6b178586a
이승우 <swoolee97@ajou.ac.kr>
2024-06-14 18:03:12 +0900

    chore: 필요없는 라우터 삭제
    수동으로 페스티벌 Api 호출하여 페스티벌 데이터베이스 업데이트하는 라우터 삭제

--- src/main/java/com/ebiz/drivel/domain/festival/api/FestivalController.java
+++ src/main/java/com/ebiz/drivel/domain/festival/api/FestivalController.java
@@ -1,7 +1,6 @@
 package com.ebiz.drivel.domain.festival.api;
 
 import com.ebiz.drivel.domain.festival.dto.FestivalDetailResponse;
-import com.ebiz.drivel.domain.festival.service.FestivalApiService;
 import com.ebiz.drivel.domain.festival.service.FestivalService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,13 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class FestivalController {
 
-    private final FestivalApiService festivalApiService;
     private final FestivalService festivalService;
-
-    @GetMapping("/test")
-    public void abc() throws Exception {
-        festivalApiService.updateFestivalData();
-    }
 
     @GetMapping("/{id}")
     public ResponseEntity<FestivalDetailResponse> getFestivalDetail(@PathVariable String id) {
