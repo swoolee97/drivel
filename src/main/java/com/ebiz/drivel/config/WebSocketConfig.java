64113d4c73e7310bb91575c3a9844f8540b01af9
이승우 <swoolee97@ajou.ac.kr>
2024-08-10 10:13:54 +0900

    refactor: 웹소켓 연결 경로 수정

--- src/main/java/com/ebiz/drivel/config/WebSocketConfig.java
+++ src/main/java/com/ebiz/drivel/config/WebSocketConfig.java
@@ -31,7 +31,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     //웹소켓 주소 설정하는 메서드
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/connect").setAllowedOriginPatterns("*");
+        registry.addEndpoint("/ws/connect").setAllowedOriginPatterns("*");
     }
 }
 
