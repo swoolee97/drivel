97e474c1a2a3dc179b5a9932346f9245762582a0
이승우 <swoolee97@ajou.ac.kr>
2024-08-11 19:04:09 +0900

    feat: 미팅 가입 요청한 유저들 확인하는 기능

--- src/main/java/com/ebiz/drivel/domain/meeting/repository/MeetingRepository.java
+++ src/main/java/com/ebiz/drivel/domain/meeting/repository/MeetingRepository.java
@@ -1,7 +1,10 @@
 package com.ebiz.drivel.domain.meeting.repository;
 
 import com.ebiz.drivel.domain.meeting.entity.Meeting;
+import com.ebiz.drivel.domain.member.entity.Member;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MeetingRepository extends JpaRepository<Meeting, Long> {
+    List<Meeting> findByMasterMemberAndIsActiveIsTrue(Member member);
 }
