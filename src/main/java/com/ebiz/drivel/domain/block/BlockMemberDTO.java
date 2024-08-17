b01c0f66e1252f18930a4f7f2201aefa0574c622
onehowon <flash0221@naver.com>
2024-08-18 00:15:48 +0900

    feat : 신고/차단 기능 중 모임 신고 기능 제거, 1:1문의 코드 수정

--- src/main/java/com/ebiz/drivel/domain/block/BlockMemberDTO.java
+++
@@ -1,8 +0,0 @@
-package com.ebiz.drivel.domain.block;
-
-import lombok.Data;
-
-@Data
-public class BlockMemberDTO {
-    private Long targetMemberId;
-}
--- src/main/java/com/ebiz/drivel/domain/report/ReportRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ebiz.drivel.domain.report;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReportRepository extends JpaRepository<ReportMember, Long> {
-}
