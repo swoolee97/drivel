ce699099ae19769482cf11ec7e1c80795fb96910
이승우 <swoolee97@ajou.ac.kr>
2024-08-31 22:48:10 +0900

    add: 코스 정보에 tag 추가

--- src/main/java/com/ebiz/drivel/domain/course/dto/CourseDetailDTO.java
+++ src/main/java/com/ebiz/drivel/domain/course/dto/CourseDetailDTO.java
@@ -1,6 +1,14 @@
 package com.ebiz.drivel.domain.course.dto;
 
 import com.ebiz.drivel.domain.course.entity.Course;
+import com.ebiz.drivel.domain.course.entity.CourseStyle;
+import com.ebiz.drivel.domain.course.entity.CourseTheme;
+import com.ebiz.drivel.domain.course.entity.CourseTogether;
+import com.ebiz.drivel.domain.onboarding.entity.Style;
+import com.ebiz.drivel.domain.onboarding.entity.Together;
+import com.ebiz.drivel.domain.theme.entity.Theme;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
@@ -9,11 +17,19 @@ import lombok.experimental.SuperBuilder;
 public class CourseDetailDTO extends CourseDTO {
     private int reviewCount; // 이거랑
     private double averageRating; // 이거는 필요없는데 상속하는게 좋을까
+    private List<String> tags;
 
     public static CourseDetailDTO from(Course course, boolean isLiked) {
+        List<String> tags = new ArrayList<>();
+        tags.addAll(course.getCourseStyles().stream().map(CourseStyle::getStyle).map(Style::getDisplayName).toList());
+        tags.addAll(course.getCourseThemes().stream().map(CourseTheme::getTheme).map(Theme::getDisplayName).toList());
+        tags.addAll(course.getCourseTogethers().stream().map(CourseTogether::getTogether).map(Together::getDisplayName)
+                .toList());
+
         return CourseDetailDTO.builder()
                 .id(course.getId())
                 .liked(isLiked)
+                .tags(tags)
                 .title(course.getTitle())
                 .description(course.getDescription())
                 .reviewCount(course.countReviews())
