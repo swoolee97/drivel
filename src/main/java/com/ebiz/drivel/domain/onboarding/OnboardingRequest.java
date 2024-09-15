c71dd4f4346abb37f2917cabb16838b93c1739f1
이승우 <swoolee97@ajou.ac.kr>
2024-09-15 18:57:35 +0900

    chore: 온보딩 개수 제한 해제

--- src/main/java/com/ebiz/drivel/domain/onboarding/OnboardingRequest.java
+++ src/main/java/com/ebiz/drivel/domain/onboarding/OnboardingRequest.java
@@ -8,10 +8,10 @@ import lombok.Getter;
 public class OnboardingRequest {
     @Size(min = 1, max = 3, message = "3개까지 선택 가능합니다")
     private List<Long> regionIds;
-    @Size(min = 1, max = 3, message = "3개까지 선택 가능합니다")
+    @Size(min = 1, message = "1개 이상 선택해주세요")
     private List<Long> togetherIds;
-    @Size(min = 1, max = 3, message = "3개까지 선택 가능합니다")
+    @Size(min = 1, message = "1개 이상 선택해주세요")
     private List<Long> themeIds;
-    @Size(min = 1, max = 3, message = "3개까지 선택 가능합니다")
+    @Size(min = 1, message = "1개 이상 선택해주세요")
     private List<Long> styleIds;
 }
