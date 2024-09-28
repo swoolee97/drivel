436815f7bfd7324c7e02b8ada2f80dd9a81155f6
이승우 <swoolee97@ajou.ac.kr>
2024-09-28 17:51:01 +0900

    add: 페스티벌 상세 데이터에 추가정보 추가

--- src/main/java/com/ebiz/drivel/domain/festival/dto/FestivalDetailResponse.java
+++ src/main/java/com/ebiz/drivel/domain/festival/dto/FestivalDetailResponse.java
@@ -18,6 +18,21 @@ public class FestivalDetailResponse {
     private BigDecimal latitude;
     private BigDecimal longitude;
     private String description;
+    private String sponsor;
+    private String sponsorTel;
+    private String playTime;
+    private String eventPlace;
+    private String eventHomepage;
+    private String ageLimit;
+    private String bookingPlace;
+    private String placeInfo;
+    private String subEvent;
+    private String program;
+    private String useTimeFestival;
+    private String spendTimeFestival;
+    private String festivalGrade;
+    private String infoName;
+    private String infoText;
 
     public static FestivalDetailResponse from(Festival festival) {
         return FestivalDetailResponse.builder()
@@ -31,6 +46,21 @@ public class FestivalDetailResponse {
                 .latitude(festival.getLatitude())
                 .longitude(festival.getLongitude())
                 .description(festival.getDescription())
+                .sponsor(festival.getSponsor())
+                .sponsorTel(festival.getSponsorTel())
+                .playTime(festival.getPlayTime())
+                .eventPlace(festival.getEventPlace())
+                .eventHomepage(festival.getEventHomepage())
+                .ageLimit(festival.getAgeLimit())
+                .bookingPlace(festival.getBookingPlace())
+                .placeInfo(festival.getPlaceInfo())
+                .subEvent(festival.getSubEvent())
+                .program(festival.getProgram())
+                .useTimeFestival(festival.getUseTimeFestival())
+                .spendTimeFestival(festival.getSpendTimeFestival())
+                .festivalGrade(festival.getFestivalGrade())
+                .infoName(festival.getInfoName())
+                .infoText(festival.getInfoText())
                 .build();
     }
 
